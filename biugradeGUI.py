1366a1da6e9903924f2b928e4798a57d791a17b2
Nizan Mandelblit <47861710+NizanMandelblit@users.noreply.github.com>
2021-02-18 20:21:14 +0200

    Update biugradeGUI.py

--- biugradeGUI.py
+++ biugradeGUI.py
@@ -1,14 +1,18 @@
 import tkinter
+import webbrowser
 from tkinter import *
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+
+
+def callback():
+    webbrowser.open_new("https://github.com/NizanMandelblit/BIU-grades-scraping")
 
 
 def main():
     window = Tk()
     window.geometry("500x500")
     window.title("BIU inbar auto grades")
-
     biuphoto = PhotoImage(file="BIULOGO.png")
     photo = Label(window, image=biuphoto, bg="black").grid(row=0, column=0, sticky=W, pady=2, columnspan=5, rowspan=2)
     tkinter.Label(window, text="Id").grid(row=3, column=0, sticky=W, pady=2)
@@ -18,19 +22,23 @@ def main():
     e1.grid(row=3, column=1, sticky=W, pady=2)
     e2.grid(row=4, column=1, sticky=W, pady=2)
 
+    e1.insert(0, 'enter id')
+    e2.insert(0, 'enter pass')
+
     def buttonClick():
         id = e1.get()
         pwd = e2.get()
         op = webdriver.ChromeOptions()
         op.add_argument('headless')
-        chromedriver = "c:/Users/cellium/Downloads/chromedriver.exe"
+        op.add_experimental_option('excludeSwitches', ['enable-logging'])
+        chromedriver = "chromedriver.exe"
         driver = webdriver.Chrome(chromedriver, options=op)
         driver.get("https://inbar.biu.ac.il/Live/Login.aspx")
-        driver.implicitly_wait(1)
+        driver.implicitly_wait(3)
         driver.find_element_by_name("edtUsername").send_keys(id)
         driver.find_element_by_name("edtPassword").send_keys(pwd)
-        driver.find_element_by_css_selector("#btnLogin").click()
-        driver.implicitly_wait(1)
+        driver.find_element_by_name("btnLogin").click()
+        driver.implicitly_wait(3)
         driver.find_element_by_css_selector("#tvMainn7").click()
         driver.find_element_by_css_selector("#tvMainn7Nodes > table:nth-child(2)").click()
         table_id = driver.find_element(By.ID, 'ContentPlaceHolder1_gvGradesList')
@@ -53,6 +61,9 @@ def main():
 
     button1 = tkinter.Button(window, text="submit", command=buttonClick)
     button1.grid(columnspan=2, row=5, column=0)
+    link1 = tkinter.Button(window, text="Made by Nizan Mandelblit", fg="blue", cursor="hand2", command=callback).grid(
+        row=10, column=1)
+
     window.mainloop()
 
 
